8bed43d9b96bb729de077685ea4a87900b27383a
seonyoungan <asy093@naver.com>
2022-03-25 01:56:03 +0900

    update study basic

---
+++ 2439.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    
+    for(int i=0;i<a;i++){
+        for(int j=1;j<a-i;j++){
+            printf(" ");
+        }
+        
+        for(int k=0;k<i+1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
